a7e0b42842d44a91a5d0785530d6a9ead8bc923b
Dhruv Sapra <saprad@vcu.edu>
2020-09-18 01:04:08 -0400

    Added activity and on click listener for a registration page to create account and function to check if fields have values.

--- app/src/main/java/com/example/my2cents/MainActivity.java
+++ app/src/main/java/com/example/my2cents/MainActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText User;
+    EditText Pass;
+    TextView Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User = (EditText)findViewById(R.id.userEditText);
+        Pass = (EditText)findViewById(R.id.pwEditText);
+
+        Register = (TextView)findViewById(R.id.fpwTextView);
 
         Button loginBtn = (Button) findViewById(R.id.loginBtn);
 
@@ -23,5 +31,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/my2cents/RegisterActivity.java
@@ -0,0 +1,56 @@
+package com.example.my2cents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText createUsername;
+    private EditText createPassword;
+    private EditText createEmail;
+    private Button confirm;
+    private TextView goBack;
+
+    // add firebase here and imports on top
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        setupIds();
+
+        }
+
+    private void setupIds() {
+        createUsername = (EditText) findViewById(R.id.etName);
+        createPassword = (EditText) findViewById(R.id.etPassword);
+        createEmail = (EditText) findViewById(R.id.etEmail);
+        confirm = (Button) findViewById(R.id.btnRegister);
+        goBack = (TextView) findViewById(R.id.tvGoback);
+    }
+    private Boolean checkboxes(){
+        Boolean emptyBox = false;
+
+        String nameBox = createUsername.getText().toString();
+        String passBox  = createPassword.getText().toString();
+        String emailBox = createEmail.getText().toString();
+
+
+        if (nameBox.isEmpty() && passBox.isEmpty() && emailBox.isEmpty()) {
+            Toast.makeText(this, "Please enter your information", Toast.LENGTH_SHORT).show();
+
+
+        }
+        else {
+            emptyBox = true;
+
+        }
+        return emptyBox;
+    }
+}
