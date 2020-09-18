8496fc6bfce317e91542e3a056c7384d4d7aa0b1
Colden <grietzercs@vcu.edu>
2020-09-17 23:33:22 -0400

    test branch commit

--- app/src/main/java/com/example/my2cents/MainActivity.java
+++ app/src/main/java/com/example/my2cents/MainActivity.java
@@ -28,28 +28,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                /* Comment the lines */
-                String correct_username = "admin";
-                String correct_password = "password";
-                //validate inputs
-                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())) {
-                    Toast.makeText(MainActivity.this, "Insufficient data provided", Toast.LENGTH_LONG).show();
-                }
-                else if (username.getText().toString().equals(correct_username)) {
-                    //check password
-                    if (password.getText().toString().equals(correct_password)) {
-                        Toast.makeText(MainActivity.this, "Successful Login",Toast.LENGTH_LONG).show();
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class); //comment out these lines if enabling authentication
+                startActivity(startIntent); //comment out these lines if enabling authentication
 
-                        //It will move to next screen after successful sign on
-                        Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
-                        startActivity(startIntent);
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, "Invalid username/password",Toast.LENGTH_LONG).show();
-                    }
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Invalid username/password",Toast.LENGTH_LONG).show();
+                /******************** Uncomment the following lines to enable username and password authentication in cooperation with "login" button **************************************************/
+//                String correct_username = "admin";
+//                String correct_password = "password";
+//                //validate inputs
+//                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())) {
+//                    Toast.makeText(MainActivity.this, "Insufficient data provided", Toast.LENGTH_LONG).show();
+//                }
+//                else if (username.getText().toString().equals(correct_username)) {
+//                    //check password
+//                    if (password.getText().toString().equals(correct_password)) {
+//                        Toast.makeText(MainActivity.this, "Successful Login",Toast.LENGTH_LONG).show();
+//
+//                        //It will move to next screen after successful sign on
+//                        Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+//                        startActivity(startIntent);
+//                    }
+//                    else {
+//                        Toast.makeText(MainActivity.this, "Invalid username/password",Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//                else {
+//                    Toast.makeText(MainActivity.this, "Invalid username/password", Toast.LENGTH_LONG).show();
+//                }
             }
         });
     }
