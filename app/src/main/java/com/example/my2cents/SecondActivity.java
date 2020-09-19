69e7246a3b5ad30000df1949aa0d5518484d13da
Colden <grietzercs@vcu.edu>
2020-09-19 04:07:08 -0400

    Completed functional bottom navigation manu with four screens/fragments

--- app/src/main/java/com/example/my2cents/SecondActivity.java
+++ app/src/main/java/com/example/my2cents/SecondActivity.java
@@ -1,8 +1,13 @@
 package com.example.my2cents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
+
 }
