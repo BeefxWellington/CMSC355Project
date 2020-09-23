e813a8c73f531c691507e75af1a461fc7a98cb68
Dhruv Sapra <saprad@vcu.edu>
2020-09-22 22:46:35 -0400

    Tried to create spinner for add finances fragment.

--- app/src/main/java/com/example/my2cents/Add.java
+++ app/src/main/java/com/example/my2cents/Add.java
@@ -1,12 +1,12 @@
 package com.example.my2cents;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,8 +24,11 @@ public class Add extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    Spinner InputType;
+
     public Add() {
         // Required empty public constructor
+
     }
 
     /**
@@ -52,13 +55,21 @@ public class Add extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+         //   setUpSpinner(view);
         }
     }
 
+    public void setUpSpinner(View view){
+        InputType = (Spinner) view.findViewById(R.id.spinner);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+
     }
+
+
 }
