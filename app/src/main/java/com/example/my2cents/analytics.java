24f632896428530907c7eb1fcbad8bddbd970e5e
Colden <grietzercs@vcu.edu>
2020-10-01 19:47:37 -0400

    Implemented notifications via button

--- app/src/main/java/com/example/my2cents/analytics.java
+++ app/src/main/java/com/example/my2cents/analytics.java
@@ -1,10 +1,19 @@
 package com.example.my2cents;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import com.anychart.AnyChart;
@@ -23,9 +32,17 @@ import java.util.List;
  */
 public class analytics extends Fragment {
 
+    /** pie chart variables **/
     private AnyChartView anyChartView;
     private String[] months = {"Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     private int[] expenditures = {20, 50, 100, 500, 1000, 1200, 1500, 50, 180, 200, 70, 350};
+
+    /** notifications test variables **/
+    private Button btn_notifications;
+    private final String CHANNEL_ID = "bills";
+    private final int NOTIFCATIONS_ID = 001;
+    private Context mContext;
+    private Resources mResources;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -67,11 +84,32 @@ public class analytics extends Fragment {
         }
     }
 
+    //treat this as regular onCreate to store Java code
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        btn_notifications = v.findViewById(R.id.btn_notifications);
+        btn_notifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Hello! I'm a notification!";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_money)
+                        .setContentTitle("My2Cents Notification")
+                        .setAutoCancel(true);
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("message", message);
+
+                PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(contentIntent);
+
+                createNotificationChannel(builder);
+            }
+        });
 
         anyChartView = v.findViewById(R.id.piechart);
         setupPieChart();
@@ -79,6 +117,7 @@ public class analytics extends Fragment {
         return v;
     }
 
+    //configuration of the pie chart exists here
     public void setupPieChart() {
         Pie pie = AnyChart.pie();
         List<DataEntry> dataEntries = new ArrayList<>();
@@ -100,5 +139,26 @@ public class analytics extends Fragment {
         pie.selected().outline().offset(2);
 
         anyChartView.setChart(pie);
+    }
+
+    private void createNotificationChannel(NotificationCompat.Builder builder) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "bills";
+            String description = "Includes all notifications regarding required outgoing expenses";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channelBills = new NotificationChannel(CHANNEL_ID, name, importance);
+            channelBills.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channelBills);
+            channelBills.enableLights(true);
+            channelBills.enableVibration(true);
+
+            notificationManager.notify(0, builder.build());
+        }
     }
 }
