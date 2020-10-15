da2f269d28eb70aaa2369c780ebeb0f779e08659
Dana Cantillon <cantillond@mymail.vcu.edu>
2020-10-15 00:10:41 -0400

    Make layout for log list

--- app/src/main/java/com/example/my2cents/AnalyticsLog.java
+++ app/src/main/java/com/example/my2cents/AnalyticsLog.java
@@ -4,60 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AnalyticsLog#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AnalyticsLog extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View v;
+    ListView listView;
+    String[] month;
+    String[] day;
+    String[] title;
+    String[] type;
+    int[] amount;
+    int[] balance;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public AnalyticsLog() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AnalyticsLog.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AnalyticsLog newInstance(String param1, String param2) {
-        AnalyticsLog fragment = new AnalyticsLog();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public AnalyticsLog(){
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        v = inflater.inflate(R.layout.analytics_log, container, false);
+
+        listView = v.findViewById(R.id.logListView);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.analytics_log, container, false);
+        return v;
     }
+
+
 }
