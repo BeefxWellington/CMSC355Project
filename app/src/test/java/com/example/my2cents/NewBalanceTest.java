04fee9ce41f7e5a99d686e9bc72d3a83504acf90
Dana Cantillon <cantillond@mymail.vcu.edu>
2020-10-28 19:51:51 -0400

    Create new test class to check getBalance method

---
+++ app/src/test/java/com/example/my2cents/NewBalanceTest.java
@@ -0,0 +1,14 @@
+package com.example.my2cents;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NewBalanceTest {
+
+    @Test
+    public void getBalance(){
+
+    }
+
+}
