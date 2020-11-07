168bfbf5eaf8ad0b259f1ca6c5d04d78adf7a413
yachuwbg <36456919+yachuwbg@users.noreply.github.com>
2020-11-06 21:09:21 -0500

    Updates

--- app/src/main/java/com/example/my2cents/Onboarding.java
+++ app/src/main/java/com/example/my2cents/Onboarding.java
@@ -1,24 +1,27 @@
 package com.example.my2cents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Onboarding extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
     }
 
+// on the skip button, go to home
     public void Skipped(View view) {
-        startActivity(new Intent(this,Home.class));
+        startActivity(new Intent(this,
+                Home.class));
         finish();
     }
 
+ // the next button should move to the next display
     public void next(View view) {
     }
 }
