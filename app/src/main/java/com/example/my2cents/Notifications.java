ec58884f812175e68ef1d4a047f0bd0ebd792f0f
Anna Ho <hoa7@mymail.vcu.edu>
2020-11-16 15:52:56 -0500

    Cleaned up code

--- app/src/main/java/com/example/my2cents/Notifications.java
+++ app/src/main/java/com/example/my2cents/Notifications.java
@@ -12,66 +12,41 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Notifications#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Notifications extends Fragment {
+public class Notifications extends android.app.Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    boolean incomeNotifications = true;
+    boolean expenseNotifications = true;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch all;
+    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch income;
+    @SuppressLint("UseSwitchCompatOrMaterialCode") Switch expense;
+    Button saveNotifications;
 
     public Notifications() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Notifications.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Notifications newInstance(String param1, String param2) {
-        Notifications fragment = new Notifications();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
-    private boolean incomeNotifications = true;
-    private boolean expenseNotifications = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
-        Button b1 = (Button) view.findViewById(R.id.savenotifications);
+        saveNotifications = (Button) view.findViewById(R.id.savenotifications);
 
-        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch all = (Switch) view.findViewById(R.id.switch1);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch income = (Switch) view.findViewById(R.id.switch2);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch expense = (Switch) view.findViewById(R.id.switch3);
+        all = (Switch) view.findViewById(R.id.switch1);
+        income = (Switch) view.findViewById(R.id.switch2);
+        expense = (Switch) view.findViewById(R.id.switch3);
 
 
         all.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -128,7 +103,7 @@ public class Notifications extends Fragment {
         homeFragment.setArguments(bundle);
 
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        saveNotifications.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(), "Saved!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/my2cents/PasswordSettings.java
+++ app/src/main/java/com/example/my2cents/PasswordSettings.java
@@ -10,52 +10,22 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PasswordSettings#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PasswordSettings extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText newPassword;
+    EditText currentPassword;
+    EditText confirmPassword;
 
     public PasswordSettings() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PasswordSettings.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PasswordSettings newInstance(String param1, String param2) {
-        PasswordSettings fragment = new PasswordSettings();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,9 +33,9 @@ public class PasswordSettings extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_password_settings, container, false);
         Button b1 = (Button) view.findViewById(R.id.changepassword);
-        final EditText newPassword = (EditText) view.findViewById(R.id.newPasswordEditText);
-        final EditText currentPassword = (EditText) view.findViewById(R.id.currentPasswordEditText);
-        final EditText confirmPassword = (EditText) view.findViewById(R.id.confirmPasswordEditText);
+        newPassword = (EditText) view.findViewById(R.id.newPasswordEditText);
+        currentPassword = (EditText) view.findViewById(R.id.currentPasswordEditText);
+        confirmPassword = (EditText) view.findViewById(R.id.confirmPasswordEditText);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
