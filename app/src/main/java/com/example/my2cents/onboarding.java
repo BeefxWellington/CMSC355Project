926cf955462c73b13cdec3d1b651841631f157f3
yachuwbg <36456919+yachuwbg@users.noreply.github.com>
2020-11-24 09:28:40 -0500

    onboarding update

--- app/src/main/java/com/example/my2cents/onboarding.java
+++ app/src/main/java/com/example/my2cents/onboarding.java
@@ -6,20 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class onboarding extends AppCompatActivity {
 
-    //FloatingActionButton backbtn = findViewById(R.id.backbtn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
-       // backbtn.setOnClickListener(new View.OnClickListener() {
 
-
-         //   @Override
-           // public void onClick(View v) {
-             //   Intent intent = new Intent(onboarding.this, SecondActivity.class);
-            //}
-        //});
     }
 }
